4bce3b4187b4df7f82424dd8b92be3e92f8cd237
Matt Lightfoot <m-lightfoot@hotmail.com>
2025-02-01 20:51:23 +0000

    Fixed type of major order component import on app file

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 import './main.scss'
-import MajorOrder from './components/majorOrder';
+import MajorOrder from './components/MajorOrder';
 
 function App() {
 
